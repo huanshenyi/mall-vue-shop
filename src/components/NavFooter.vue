09242f4c3f8743acb13ecfbb6a0cd56d7edffd87
huanshenyi <txy1226052@gmail.com>
2020-01-08 23:01:24 +0900

    footer ok

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>NavFooter</div>
+    <div class="footer">
+        <div class="footer-logo">
+            <img src="/imgs/logo-footer.png" alt="">
+            <p>MiShop</p>
+        </div>
+        <div class="footer-link">
+            <a href="" target="_blank">GiyHub</a><span>|</span>
+            <a href="" target="_blank">GiyHub</a><span>|</span>
+            <a href="" target="_blank">GiyHub</a><span>|</span>
+            <a href="" target="_blank">GiyHub</a><span>|</span>
+        </div>
+        <div class="copyright">バージョン管理ドメイン管理など</div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +20,32 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.footer{
+    height: 234px;
+    border-top: 4px solid #FF6600;
+    background-color: #333333;
+    color: #999999;
+    font-size: 16px;
+    text-align: center;
+    .footer-logo{
+        margin-top: 46px;
+        margin-bottom: 31px;
+        img{
+            width: 53px;
+            height: 36px;
+            margin-bottom: 13px;
+        }
+    }
+    .footer-link{
+        a{
+            color: #999999;
+            display: inline-block;
+        }
+        span{
+            margin: 0 10px;
+        }
+        margin-bottom: 13px;
+    }
+}
 </style>
