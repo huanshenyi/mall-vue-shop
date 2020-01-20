44408719b668387421cc556d9eaf5de447e6c6e4
huanshenyi <txy1226052@gmail.com>
2020-01-21 00:18:03 +0900

    swiperの左側のメニュー追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,35 @@
     <div class="index">
         <div class="container">
             <div class="swiper-box">
+                <div class="nav-menu">
+                    <ul class="menu-wrap">
+                        <li class="menu-item">
+                            <a href="javascript:;">スマホ Cimカード</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">テレビ 何か</a>
+                        </li>
+                         <li class="menu-item">
+                            <a href="javascript:;">テレビ 何か</a>
+                        </li>
+                         <li class="menu-item">
+                            <a href="javascript:;">ノートパソコン</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">何か 何か</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">ルーター1</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">ルーター2</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">ルーター3</a>
+                        </li>
+                    </ul>
+                </div>
                 <swiper :options="swiperOption">
                    <swiper-slide v-for="(item, index) in slideList" :key="index">
                        <a :href="'/#/product/'+item.id"><img :src="item.img" alt=""></a>
@@ -83,11 +112,47 @@
     }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+@import "./../assets/scss/mixin.scss";
 .index{
     .swiper-box{
+        .nav-menu {
+            position: absolute;
+            width: 264px;
+            height: 451px;
+            z-index: 9;
+            padding: 26px 0;
+            background-color:#55585a7a ;
+            box-sizing: border-box;
+            .menu-wrap{
+                .menu-item{
+                    height: 50px;
+                    line-height: 50px;
+                    a{
+                        display: block;
+                        position: relative;
+                        font-size: 16px;
+                        color: #ffffff;
+                        padding-left: 30px;
+                        &:after{
+                          position: absolute;
+                          right: 30px;
+                          top: 17.5px;
+                          content: " ";
+                          @include bgImg(10px,15px, '/imgs/icon-arrow.png')
+                       }
+                    }
+                    &:hover{
+                        background-color: #FF6600;
+                    }
+                }
+            }
+        }
         .swiper-container {
             height: 451px;
+            .swiper-button-prev{
+                left: 274px;
+            }
             img {
                 width: 100%;
                 height: 100%;
